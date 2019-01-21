46cb88578d717e696cbe8b057b399f4bbd22113a
Sean Passmore <jeanghandi@gmail.com>
2019-01-21 02:54:10 +0000

    Completed task 3 with the new div tags

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,9 +1,19 @@
 class SayController < ApplicationController
-  def hello
-   @time = Time.now
-  end
-  def filenames
-    @files =Dir.glob('*')
-  def goodbye
-  end
+    before_action :time_now
+
+    def hello
+    end
+
+    def goodbye
+    end
+
+    def filenames
+        @files = Dir.glob('*')
+    end
+
+    private
+
+    def time_now
+        @time = Time.now
+    end
 end
